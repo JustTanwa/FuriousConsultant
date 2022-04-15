f09326ef2f21a144e9b60a0a22eb19c094447407
Katakung95 <tanwasripan@hotmail.co.uk>
2022-04-15 19:22:43 +0100

    Landing page completed with mobile responsive

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Furious Consultant
 
-This is a small project website for me to learn about creating landing page and web site layout. I will be using the a fictional company and characters from the Fast and Furious movie franchise. 
+This is a small project website for me to learn about creating landing page and web site layout. I will be using the a fictional company and characters from the Fast and Furious movie franchise.
 
 ## Tech Stack
 
 I will be using HTML5, CSS, JavaScript and little jQuery for this project.
+
+I will also be using Font Awesome v5 for some icons.
 
 ### Goals
 
@@ -14,8 +16,11 @@ I want to have a website with a CTA button on the landing page. As well as explo
 
 The first challenge I encounter was how to make the hamburger menu remain fixed while the menu is open, and disable user scroll. I wanted this feature so that when the user open up the navigation menu they will not be able to scroll the rest of the content. After some research, I found the solution, which was to place the body element of the HTML to be fixed. I achieved this using jQuery as I had already set up toggle for the opening and closing of the burger menu.
 
+The next challenge was creating a simple image carousel from scratch using javascript. Creating this element, I solidify my knowledge with DOM manipulation and using Event Listeners for the carousel buttons.
+
 ### Credits
 
 I will be using TMDb for the movie database information and images will be taken from Unsplash.
+
 - Hero image from Unsplash by Shiro Hatori
 - Image of the laughing man from Pixabay, artist unknown.
--- index.js
+++ index.js
@@ -9,14 +9,64 @@ $(document).ready(function () {
 
 // Name on image
 $(window).resize(function () {
-    if (window.innerWidth > 920) {
-        $(".intro-name").html(`<pre>Hi,
-I'm Dominic</pre>`)
-    } else {
-        $(".intro-name").html(`<pre>Hi,
+	if (window.innerWidth > 920) {
+		$('.intro-name').html(`<pre>Hi,
+I'm Dominic</pre>`);
+	} else {
+		$('.intro-name').html(`<pre>Hi,
 I'm 
-Dominic</pre>`)
-    }
+Dominic</pre>`);
+	}
 });
 
+// carousel
 
+const slides = document.getElementsByClassName('testimonial-card');
+const nextBtn = document.getElementById('test-next');
+const prevBtn = document.getElementById('test-prev');
+let curSlide = 0;
+
+const switchSlides = (currentSlide, nextSlide) => {
+	currentSlide.classList.remove('active');
+	nextSlide.classList.add('active');
+};
+
+nextBtn.addEventListener('click', () => {
+	if (curSlide < slides.length - 1) {
+		const currentSlide = slides[curSlide];
+		const nextSlide = slides[curSlide + 1];
+		switchSlides(currentSlide, nextSlide);
+		curSlide++;
+	} else {
+		curSlide = 0;
+		const currentSlide = slides[slides.length - 1];
+		const nextSlide = slides[curSlide];
+		switchSlides(currentSlide, nextSlide);
+	}
+});
+
+prevBtn.addEventListener('click', () => {
+	if (curSlide > 0) {
+		const currentSlide = slides[curSlide];
+		const nextSlide = slides[curSlide - 1];
+		switchSlides(currentSlide, nextSlide);
+		curSlide--;
+	} else {
+		const nextSlide = slides[slides.length - 1];
+		const currentSlide = slides[curSlide];
+		switchSlides(currentSlide, nextSlide);
+		curSlide = slides.length - 1;
+	}
+});
+
+const images = [
+	'/assets/img/woman.png',
+	'/assets/img/woman2.png',
+	'/assets/img/man.png',
+];
+const cards = document.getElementsByClassName('card');
+Array.from(cards).forEach((elem, i) => {
+	console.log(elem.firstElementChild);
+	return (elem.firstElementChild.style.backgroundImage =
+		'url(' + images[i] + ')');
+});
