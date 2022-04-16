79e3fe7b58166b435fd5ac1fff9b069f744ffbe1
Katakung95 <tanwasripan@hotmail.co.uk>
2022-04-16 22:53:48 +0100

    Added request to TMDb using fetch API

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ I will be using HTML5, CSS, JavaScript and little jQuery for this project.
 
 I will also be using Font Awesome v5 for some icons.
 
+TMDb API for calling the characters in the Fast and Furious movie.
+
 ### Goals
 
 I want to have a website with a CTA button on the landing page. As well as explore a couple of pages to test layout of forms and information page.
@@ -17,6 +19,8 @@ I want to have a website with a CTA button on the landing page. As well as explo
 The first challenge I encounter was how to make the hamburger menu remain fixed while the menu is open, and disable user scroll. I wanted this feature so that when the user open up the navigation menu they will not be able to scroll the rest of the content. After some research, I found the solution, which was to place the body element of the HTML to be fixed. I achieved this using jQuery as I had already set up toggle for the opening and closing of the burger menu.
 
 The next challenge was creating a simple image carousel from scratch using javascript. Creating this element, I solidify my knowledge with DOM manipulation and using Event Listeners for the carousel buttons.
+
+I did not want to hard code the information for the "about me" page so I decided to look for a database API to use so I can call it to get charater informations to display. I came across TMDb, and used the fetch API to request the data.
 
 ### Credits
 
--- index.js
+++ index.js
@@ -20,52 +20,80 @@ Dominic</pre>`);
 });
 
 // carousel
+const makeCarousel = () => {
+	const slides = document.getElementsByClassName('testimonial-card');
+	const nextBtn = document.getElementById('test-next');
+	const prevBtn = document.getElementById('test-prev');
+	let curSlide = 0;
 
-const slides = document.getElementsByClassName('testimonial-card');
-const nextBtn = document.getElementById('test-next');
-const prevBtn = document.getElementById('test-prev');
-let curSlide = 0;
+	const switchSlides = (currentSlide, nextSlide) => {
+		currentSlide.classList.remove('active');
+		nextSlide.classList.add('active');
+	};
 
-const switchSlides = (currentSlide, nextSlide) => {
-	currentSlide.classList.remove('active');
-	nextSlide.classList.add('active');
+	nextBtn.addEventListener('click', () => {
+		if (curSlide < slides.length - 1) {
+			const currentSlide = slides[curSlide];
+			const nextSlide = slides[curSlide + 1];
+			switchSlides(currentSlide, nextSlide);
+			curSlide++;
+		} else {
+			curSlide = 0;
+			const currentSlide = slides[slides.length - 1];
+			const nextSlide = slides[curSlide];
+			switchSlides(currentSlide, nextSlide);
+		}
+	});
+
+	prevBtn.addEventListener('click', () => {
+		if (curSlide > 0) {
+			const currentSlide = slides[curSlide];
+			const nextSlide = slides[curSlide - 1];
+			switchSlides(currentSlide, nextSlide);
+			curSlide--;
+		} else {
+			const nextSlide = slides[slides.length - 1];
+			const currentSlide = slides[curSlide];
+			switchSlides(currentSlide, nextSlide);
+			curSlide = slides.length - 1;
+		}
+	});
+
+	const images = [
+		'/assets/img/woman.png',
+		'/assets/img/woman2.png',
+		'/assets/img/man.png',
+	];
+	const cards = document.getElementsByClassName('card');
+	Array.from(cards).forEach((elem, i) => {
+		return (elem.firstElementChild.style.backgroundImage =
+			'url(' + images[i] + ')');
+	});
 };
+if (document.querySelector('.testimonial-card')) makeCarousel();
 
-nextBtn.addEventListener('click', () => {
-	if (curSlide < slides.length - 1) {
-		const currentSlide = slides[curSlide];
-		const nextSlide = slides[curSlide + 1];
-		switchSlides(currentSlide, nextSlide);
-		curSlide++;
-	} else {
-		curSlide = 0;
-		const currentSlide = slides[slides.length - 1];
-		const nextSlide = slides[curSlide];
-		switchSlides(currentSlide, nextSlide);
-	}
-});
+// getting actors images with TMDb
+let baseURL = 'https://api.themoviedb.org/3';
 
-prevBtn.addEventListener('click', () => {
-	if (curSlide > 0) {
-		const currentSlide = slides[curSlide];
-		const nextSlide = slides[curSlide - 1];
-		switchSlides(currentSlide, nextSlide);
-		curSlide--;
-	} else {
-		const nextSlide = slides[slides.length - 1];
-		const currentSlide = slides[curSlide];
-		switchSlides(currentSlide, nextSlide);
-		curSlide = slides.length - 1;
-	}
-});
+let url = baseURL + '/search/movie?api_key=' + APIKEY + '&query=Furious+7';
+let movieId;
 
-const images = [
-	'/assets/img/woman.png',
-	'/assets/img/woman2.png',
-	'/assets/img/man.png',
-];
-const cards = document.getElementsByClassName('card');
-Array.from(cards).forEach((elem, i) => {
-	return (elem.firstElementChild.style.backgroundImage =
-		'url(' + images[i] + ')');
-});
+async function getMovieId(url) {
+	const response = await fetch(url);
+	const data = await response.json();
+	movieId = data.results[0].id;
+	return movieId;
+}
+
+async function getMovieDetails() {
+	const movieId = await getMovieId(url);
+	console.log(movieId);
+	console.log(baseURL + '/movie/' + movieId + '/credits?api_key=' + APIKEY);
+	const response = await fetch(
+		baseURL + '/movie/' + movieId + '/credits?api_key=' + APIKEY
+	);
+	const data = await response.json();
+	console.log(data);
+}
+
+document.addEventListener('DOMContentLoaded', getMovieDetails);
